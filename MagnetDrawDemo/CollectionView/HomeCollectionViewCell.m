64ceb213575130a819c83278dfa093593bbc5c1c
Derek316x <dereknetto@gmail.com>
2015-12-05 23:14:12 -0500

    borders

--- MagnetDrawDemo/CollectionView/HomeCollectionViewCell.m
+++ MagnetDrawDemo/CollectionView/HomeCollectionViewCell.m
@@ -16,12 +16,12 @@
     UIView *bgView = [[UIView alloc]initWithFrame:self.bounds];
     self.backgroundView = bgView;
     //self.backgroundView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"blue"]];
-    self.backgroundView.backgroundColor = [UIColor colorWithRed:255.0/255.0 green:208.0/255.0 blue:0.0 alpha:1.0];
+    self.backgroundView.backgroundColor = [UIColor whiteColor];
     
     // selected background
     UIView *selectedView = [[UIView alloc]initWithFrame:self.bounds];
     self.selectedBackgroundView = selectedView;
-    self.selectedBackgroundView.backgroundColor = [UIColor redColor];
+    self.selectedBackgroundView.backgroundColor = [UIColor colorWithRed:255.0/255.0 green:208.0/255.0 blue:0.0 alpha:1.0];
     //self.selectedBackgroundView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"pink"]];
     
 }
